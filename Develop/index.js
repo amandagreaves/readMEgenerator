27e31d1c18a72935aff395e9cb071c9cbb56ff7b
Amanda Greaves <amandamgreavess@gmail.com>
2020-09-20 14:38:41 -0600

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,46 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Give a descritption of your project.",
+        },
+
+])
+}
+// array of questions for user
+
+//const questions = [
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+//function writeToFile(fileName, data) {
+
 
 // function to initialize program
-function init() {
+//function init() {
 
-}
+//}
 
 // function call to initialize program
-init();
+//init();
+
+//* Title
+//* Description
+//* Table of Contents
+//* Installation
+//* Usage
+//* License
+//* Contributing
+//* Tests
+//* Questions
