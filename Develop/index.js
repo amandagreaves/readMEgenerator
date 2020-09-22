6695327495a71554820a698692f33c9d1fc86fc8
Amanda Greaves <amandamgreavess@gmail.com>
2020-09-22 16:37:30 -0600

    replacing html with md

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown")
+//const util = require("util");
+//const writeFileAsync = util.promisify(fs.writeFile);
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-function promptUser() {
-    return inquirer.prompt([
+inquirer
+    .prompt([
     {
         type: "input",
         name: "name",
@@ -29,7 +29,7 @@ function promptUser() {
     {
         type: "list",
         name: "license",
-        message: "What license did you use?",
+        message: "What license did you use? select from the list below.",
         choices: [
             "Apache License 2.0",
             "BSD 3-Clause (New or Revised) license",
@@ -47,51 +47,35 @@ function promptUser() {
         name: "linkedin",
         message: "Enter your LinkedIn URL.",
     },
-    ]);
-}
-function generateHTML(answers) {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>README.md</title>
-</head>
-<body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-        <p class="lead">I am from ${answers.location}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-            <li class="list-group-item">My GitHub username is ${answers.github}</li>
-            <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-        </ul>
-    </div>
-</div>
-</body>
-</html>`;
-}
+    ])
 
-promptUser()
-    .then(function (answers) {
-    const html = generateHTML(answers);
-    return writeFileAsync("index.html", html);
-    })
-    .then(function () {
-    console.log("Successfully wrote to index.html");
-    })
-    .catch(function (err) {
+
+
+.then(function (answers) {
+    const input = generateMarkdown(answers);
+    writeToFile("README.md", "amanda")
+})
+//.then(function () {
+    //console.log("Successfully wrote to index.html");
+//})
+.catch(function (err) {
     console.log(err);
+});
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if(err){
+            return console.log(err);
+        }
+        console.log("Success!");
     });
+}
 // array of questions for user
 //const questions = [
-
+    
 // function to write README file
-//function writeToFile(fileName, data) {
 
+//return writeFileAsync("index.html", html);
 // function to initialize program
 //function init() {
 
